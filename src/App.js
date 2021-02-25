311f9264ad828d142a8e6892c2309255b6160232
Dorian <dorian.mendoza@csiciber.com>
2021-02-25 10:19:25 -0600

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ( { saludo } ) => {
+
+    return (
+        <>
+            <h1>{ saludo }</h1>
+            <p>mi primer</p>
+        </>
+    )
 }
 
-export default App;
+App.propTypes = {
+    saludo: PropTypes.string.isRequired
+}
+
+export default App
